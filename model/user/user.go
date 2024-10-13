9276c7b77fe779c72cda4798409a2d523c87f7d6
MUHAMMAD RIFKY <muhammad26rifky06@gmail.com>
2024-10-13 14:42:51 +0700

    model user

---
+++ model/user/user.go
@@ -0,0 +1,14 @@
+package user
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Users struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Nama     string             `bson:"nama,omitempty" json:"nama,omitempty"`
+	No_HP    string             `bson:"no_hp,omitempty" json:"no_hp,omitempty"`
+	Email    string             `bson:"email,omitempty" json:"email,omitempty"`
+	Role     string             `bson:"role,omitempty" json:"role,omitempty"`
+	Password string             `bson:"password,omitempty" json:"password,omitempty"`
+}
