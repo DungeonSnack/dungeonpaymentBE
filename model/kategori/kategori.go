25fbe588c8560cb87b95cf0e90649401fd8c447e
MUHAMMAD RIFKY <muhammad26rifky06@gmail.com>
2024-10-14 21:43:48 +0700

    update kategori

--- model/kategori/kategori.go
+++ model/kategori/kategori.go
@@ -9,4 +9,5 @@ type Kategori struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	NamaKategori string             `json:"nama_kategori" bson:"nama_kategori"`
 	CreatedAt   time.Time           `json:"created_at" bson:"created_at"`
+	UpdatedAt 	time.Time 			`bson:"updatedAt,omitempty" json:"updatedAt,omitempty"` 
 }
