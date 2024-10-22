01464e4083f758d27d3f641182fdc593b8fefa1b
Zamy03 <ukasyzzam@gmail.com>
2024-10-22 17:28:51 +0700

    moved to dummy, cause will update the application later

--- routes/router.go
+++ routes/router.go
@@ -2,11 +2,6 @@ package routes
 
 import (
 	"dungeonSnackBE/controller/auth"
-	"dungeonSnackBE/controller/kategori" // Ensure this path is correct and the package exists
-	"dungeonSnackBE/controller/order"
-	"dungeonSnackBE/controller/produk"
-	"dungeonSnackBE/controller/profil"
-	"dungeonSnackBE/controller/toko"
 
 	"github.com/gorilla/mux"
 )
@@ -17,29 +12,6 @@ func InitializeRoutes() *mux.Router {
 	// Define your routes here
 	router.HandleFunc("/regis", auth.Register).Methods("POST")
 	router.HandleFunc("/login", auth.Login).Methods("POST")
-
-	router.HandleFunc("/profil", profil.GetProfile).Methods("GET")
-	router.HandleFunc("/profil/update", profil.UpdateProfile).Methods("PUT")
-
-	router.HandleFunc("/addKategori", kategori.AddKategori).Methods("POST")
-	router.HandleFunc("/kategori", kategori.GetKategori).Methods("GET")
-	router.HandleFunc("/kategori", kategori.DeleteKategori).Methods("DELETE")
-	router.HandleFunc("/kategori", kategori.Updatekategori).Methods("PUT")
-
-	router.HandleFunc("/kategori/produk", produk.AddProduk).Methods("POST")
-	router.HandleFunc("/kategori/{slug}/produk", produk.GetProdukByKategori).Methods("GET")
-	router.HandleFunc("/kategori/produk/update", produk.UpdateProduk).Methods("PUT")
-	router.HandleFunc("/kategori/{slug}/produk", produk.DeleteProduk).Methods("DELETE")
-
-	router.HandleFunc("/toko", toko.GetToko).Methods("GET")
-	router.HandleFunc("/toko", toko.AddToko).Methods("POST")
-	router.HandleFunc("/toko/update", toko.UpdateToko).Methods("PUT")
-	router.HandleFunc("/toko/delete", toko.DeleteToko).Methods("DELETE")
-
-	router.HandleFunc("/order", order.GetOrder).Methods("GET")
-	router.HandleFunc("/order", order.CreateOrder).Methods("POST")
-	router.HandleFunc("/order/update", order.UpdateOrder).Methods("PUT")
-	router.HandleFunc("/order/delete", order.DeleteOrder).Methods("DELETE")
 
 	return router
 }
